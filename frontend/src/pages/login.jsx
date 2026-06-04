f6142fd78bcef31afd8dcd513a57aa76f1b6480e
jagadishannaboina <annaboinajagadish@gmail.com>
2026-06-04 06:43:34 +0530

    fixed production urls

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -25,7 +25,7 @@ function Login() {
 
         try {
             const response = await axios.post(
-                "http://localhost:5000/api/auth/login",
+                "https://ai-study-board.onrender.com/api/auth/login",
                 formData
             );
 
